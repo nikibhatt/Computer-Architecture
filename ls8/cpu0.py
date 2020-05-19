4cf05f4a43ef3ea2ef2cd54dabc77728cc85ab83
nikibhatt <nkibhatt@users.noreply.github.com>
2020-05-19 16:13:31 -0700

    completed day 2 assignment

--- ls8/cpu0.py
+++ ls8/cpu0.py
@@ -15,15 +15,22 @@ class CPU:
 
         address = 0
 
-        with open(sys.argv[1]) as f:
-            for line in f:
-                string_val = line.split("#")[0].strip()
-                if string_val == '':
-                    continue
-                v = int(string_val, 2)
-                #print(v)
-                self.ram[address] = v
-                address += 1
+        # For now, we've just hardcoded a program:
+
+        program = [
+            # From print8.ls8
+            0b10000010, # LDI R0,8
+            0b00000000,
+            0b00001000,
+            0b01000111, # PRN R0
+            0b00000000,
+            0b00000001, # HLT
+        ]
+
+        for instruction in program:
+            self.ram[address] = instruction
+            address += 1
+
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -59,48 +66,24 @@ class CPU:
         halted = False
         pc = 0
 
-        ADD = 0b10100000
-        SUB = 0b10100001
-        MUL = 0b10100010
-        DIV = 0b10100011
-        MOD = 0b10100100
-        INC = 0b01100101
-        DEC = 0b01100110
-        CMP = 0b10100111
-        AND = 0b10101000
-        NOT = 0b01101001
-        OR = 0b10101010
-        XOR = 0b10101011
-        SHL = 0b10101100
-        SHR = 0b10101101
-        NOP = 0b00000000
-        HLT = 0b00000001
-        LDI = 0b10000010
-        LD = 0b10000011
-        ST = 0b10000100
-        PUSH = 0b01000101
-        POP = 0b01000110
-        PRN = 0b01000111
-        PRA = 0b01001000
-
         while not halted:
             instruction = self.ram[pc]
 
-            if instruction == LDI: #LDI
+            if instruction == 0b10000010:
                 reg_num = self.ram[pc + 1]
                 value = self.ram[pc + 2]
                 self.registers[reg_num] = value
                 pc += 3
-            elif instruction == PRN:
+            elif instruction == 0b01000111:
                 reg_num = self.ram[pc + 1]
                 print(self.registers[reg_num])
                 pc += 2
-            elif instruction == MUL:
-                reg_num_a = self.ram[pc + 1]
-                reg_num_b = self.ram[pc + 2]
-                self.registers[reg_num_a] *= self.registers[reg_num_b]
-                pc += 3
-            elif instruction == HLT:
+            # elif instruction == ADD:
+            #     reg_num_a = ram[pc + 1]
+            #     reg_num_b = ram[pc + 2]
+            #     registers[reg_num_a] += registers[reg_num_b]
+            #     pc += 3
+            elif instruction == 0b00000001:
                 halted = True
             else:
                 print(f'unknown instruction {instruction} at address {pc}')
